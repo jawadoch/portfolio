71823ea106780ebcaf63c11c6bacc365f410ac54
jawad_ochbar <jawad.ocharag10@gmail.com>
2023-01-02 20:46:22 +0100

    deleted some files not important and creat a contact page and c.html for input data in jason file

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -2,7 +2,7 @@
 * PHP Email Form Validation - v3.5
 * URL: https://bootstrapmade.com/php-email-form/
 * Author: BootstrapMade.com
-*/
+
 (function () {
   "use strict";
 
@@ -79,3 +79,4 @@
   }
 
 })();
+*/
